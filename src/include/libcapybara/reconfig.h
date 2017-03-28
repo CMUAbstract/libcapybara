47716016e0db0cbd3fe2b46e57eaa63c60dc57b4
Alexei Colin <ac@alexeicolin.com>
2017-03-27 21:55:59 -0400

    add max values for config params as macro

--- src/include/libcapybara/reconfig.h
+++ src/include/libcapybara/reconfig.h
@@ -2,10 +2,14 @@
 #define LIBCAPYBARA_RECONFIG_H
 
 #include <stdint.h>
+#include <libmcppot/mcp4xxx.h>
 
 // The following is the lowest-level interface into the reconfigurable power
 // system. The units here are bank set and wiper setting, not voltage and
 // farads, which are a higher-level concept unnecessary at runtime.
+
+#define CAPYBARA_NUM_BANKS 4
+#define CAPYBARA_MAX_THRES POT_RESOLUTION // wiper settings
 
 // Bitmask identifying a set of capacitor banks
 typedef uint16_t capybara_bankmask_t;
