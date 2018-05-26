c2b1cd3ad2be5b5d4afc119001620f03ba0d9048
Alexei Colin <ac@alexeicolin.com>
2018-05-26 10:59:59 -0400

    capy_board_init: remove reponsibilities of the top level
    To prevent libcapybara becoming kitchen sink, its scope should
    be limited to (reconfigurable) power system, not common code
    for a particular board. Also, unlocking GPIOs, setting up
    system clock, initializing buses, etc is not the responsibility of any
    library -- this is code that should be owned by main(), because
    multiple libraries are affected by this code. The plan is to
    move more stuff out of libcapybara in separate commits.
    
    If it is desirable to put some of this initialization code that's common
    to a specific board (e.g. the Capybara board), in order to avoid
    duplicating code across apps, then a new library for this very purpose
    should be created. Libcapybara has a different purpose.

--- src/capy_board_init.c
+++ src/capy_board_init.c
@@ -13,7 +13,6 @@
 //#include <libmspuartlink/uartlink.h>
 
 #if (BOARD_MAJOR == 1 && BOARD_MINOR == 1) || BOARD_MAJOR == 2
-#warning Adding libfxl
 #include <libfxl/fxl6408.h>
 #endif// BOARD_{MAJOR,MINOR}
 
@@ -30,19 +29,18 @@
     TODO add this to libcapybara...
 */
 void capy_board_init(void) {
-    msp_watchdog_disable();
-    msp_gpio_unlock();
-		__enable_interrupt();
+
 // Don't wait if we're on continuous power
 #ifndef LIBCAPYBARA_CONT_POWER
 #pragma message ("continuous power not defined!")
-capybara_wait_for_supply();
+    capybara_wait_for_supply();
 #if (BOARD_MAJOR == 1 && BOARD_MINOR == 1) || BOARD_MAJOR == 2
-   capybara_wait_for_vcap();
+    capybara_wait_for_vcap();
 #endif // BOARD_{MAJOR,MINOR}
 #endif // LIBCAPYBARA_CONT_POWER
+
     capybara_config_pins();
-    msp_clock_setup();
+
 // Set up deep_discharge stop
 #ifndef LIBCAPYBARA_CONT_POWER
 #if BOARD_MAJOR == 1 && BOARD_MINOR == 0
@@ -53,6 +51,12 @@ capybara_wait_for_supply();
     }
 #endif //BOARD.{MAJOR,MINOR}
 #endif //LIBCAPYBARA_CONT_POWER
+
+    // TODO: the following does not belong in libcapybara, it is not related to
+    // the power system. It belongs at the top level: in the app, or a separate
+    // library with board configuration. Libcapybara scope should be kept as a
+    // library for the (reconfigurable) power system, not for the particular
+    // board (specific sensors, debug pins, etc.)
 
 #if BOARD_MAJOR == 1 && BOARD_MINOR == 0
     GPIO(PORT_SENSE_SW, OUT) &= ~BIT(PIN_SENSE_SW);
@@ -68,38 +72,30 @@ capybara_wait_for_supply();
     P3DIR |= BIT0;
     GPIO(PORT_DEBUG, OUT) &= ~BIT(PIN_DEBUG);
     GPIO(PORT_DEBUG, DIR) |= BIT(PIN_DEBUG);
-#elif (BOARD_MAJOR == 1 && BOARD_MINOR == 1)
+#elif (BOARD_MAJOR == 1 && BOARD_MINOR == 1) || BOARD_MAJOR == 2
 
-    INIT_CONSOLE();
-    __enable_interrupt();
-    msp_gpio_unlock();
     LOG2("Setting up i2c\r\n");
     EUSCI_B_I2C_setup();
     LOG2("fxl init\r\n");
     fxl_init();
+
+#if (BOARD_MAJOR == 1 && BOARD_MINOR == 1)
     fxl_out(BIT_PHOTO_SW);
     fxl_out(BIT_RADIO_SW);
     fxl_out(BIT_RADIO_RST);
     fxl_out(BIT_APDS_SW);
     fxl_pull_up(BIT_CCS_WAKE);
-    LOG2("Done fxl!\r\n");
 
     // SENSE_SW is present but is not electrically correct: do not use.
+
 #elif BOARD_MAJOR == 2
-  #warning BOARD_MAJOR is 2
-  INIT_CONSOLE();
-    __enable_interrupt();
-    msp_gpio_unlock();
-    LOG2("Setting up i2c\r\n");
-    EUSCI_B_I2C_setup();
-    LOG2("fxl init\r\n");
-    fxl_init();
     LOG2("SENSE_SW\r\n");
     fxl_out(BIT_SENSE_SW);
+#endif // BOARD VERSION 1.1 and 2
+
+    LOG2("Done fxl!\r\n");
 
 #else // BOARD_{MAJOR,MINOR}
 #error Unsupported board: do not know what pins to configure (see BOARD var)
 #endif // BOARD_{MAJOR,MINOR}
-    __enable_interrupt();
 }
-
